019a84b514204a5f494d7e11e1855140245f2308
Charles Wang <zwcharl@gmail.com>
2025-07-08 01:07:25 -0700

    progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main() {
   std::cout << std::format("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
   for (int y = 0; y < IMAGE_HEIGHT; y++) {
+    std::clog << std::format("\rScanlines remaining: {} ", IMAGE_HEIGHT - 1) << std::flush;
+
     for (int x = 0; x < IMAGE_WIDTH; x++) {
       double r = static_cast<double>(x) / (IMAGE_WIDTH - 1);
       double g = static_cast<double>(y) / (IMAGE_HEIGHT - 1);
@@ -22,6 +24,9 @@ int main() {
       std::cout << std::format("{} {} {}\n", scaled_r, scaled_g, scaled_b);
     }
   }
+
+  // Extra spaces are a hack to overwrite previous text
+  std::clog << "\rDone.                       \n";
 
   return 0;
 }
